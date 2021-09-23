d362f7b53be27548f91d2126ddbe5f5a5490dce5
Francis Kinyae <fmkinyae@gmail.com>
2021-09-23 12:53:30 +0300

    add url for homepage , login and signup

--- hood/urls.py
+++ hood/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.welcome, name='welcome')
+    #path('', views.login_view, name='login'),
+    path('',views.welcome, name='welcome'),
+    path('signup/', views.usersignup, name='signup'),
+    path('activate/<uidb64>/<token>/',views.activate_account, name='activate'),
+    path('registration/', include('django_registration.backends.activation.urls')),
+    path('registration/', include('django.contrib.auth.urls')),
+    
 ]
