36a4547718a54eb63d5503f36e998890e1a7e68e
Francis Kinyae <fmkinyae@gmail.com>
2021-09-23 12:52:55 +0300

    add login and signup views

--- hood/views.py
+++ hood/views.py
@@ -1,5 +1,83 @@
 from django.shortcuts import render
+from .forms import UserSignUpForm
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_text,force_bytes,DjangoUnicodeDecodeError
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse,reverse_lazy
+from .token_generator import account_activation_token
+from django.core.mail import EmailMessage
+from  django.http import HttpResponse,Http404
+from django.contrib.auth import authenticate,login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
+@login_required(login_url='/registration/login/')
 def welcome(request):
     return render(request, "index.html")
+
+#Usersignup view
+def usersignup(request):
+    if request.method == 'POST':
+        form = UserSignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            
+            uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+            domain = get_current_site(request).domain
+            link=reverse('activate',kwargs={'uidb64':uidb64,'token':account_activation_token.make_token(user)})
+            activate_url='http://'+domain+link
+            
+            email_body='Hi ' +user.username+ ' Please use this link to verify your account\n' +activate_url
+            
+            email_subject = 'Activate Your Account'
+            
+            to_email = form.cleaned_data.get('email')
+            
+            email = EmailMessage(email_subject, email_body, 'francis.kinyae@student.moringaschool.com',[to_email])
+            
+            email.send()
+            
+            return HttpResponse('We have sent you an email, please confirm & activate your email address to complete registration')
+    else:
+        
+        form = UserSignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+def activate_account(request, uidb64, token):
+    try:
+        uid = force_bytes(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return HttpResponse('Your account has been activate successfully')
+        
+    else:
+        return HttpResponse('Activation link is invalid!')
+    
+    
+    
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print(username,password)
+        
+        user = authenticate(request, password=password, username=username)
+        if user is None:
+            context = {"error": "Invalid username or password"}
+            return render(request, "registration/login.html",context)
+        login(request,user)
+    return render(request, "registration/login.html")
+    
+
+
