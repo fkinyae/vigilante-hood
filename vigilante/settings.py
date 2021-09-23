8f1a4dd4d4cc14e3cb466bea94841632355d9632
Francis Kinyae <fmkinyae@gmail.com>
2021-09-23 13:02:18 +0300

    add site settings

--- vigilante/settings.py
+++ vigilante/settings.py
@@ -124,8 +124,20 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'francis.kinyae@student.moringaschool.com'
+EMAIL_HOST_PASSWORD = 'KINYAEH001'
+EMAIL_PORT = 587
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/' 
