4624cfc3c4e720d7e67e4e3d888daa195099ac65
Francis Kinyae <fmkinyae@gmail.com>
2021-09-23 09:56:29 +0300

    add url for the registration views

--- vigilante/urls.py
+++ vigilante/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('hood.urls')),
+    path('accounts/', include('registration.backends.simple.urls'))
 ]
