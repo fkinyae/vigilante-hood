58f1972b22e69a4c6dbb0b4d723e0ef5b319d7b3
Francis Kinyae <fmkinyae@gmail.com>
2021-09-23 12:51:57 +0300

    add app url

--- vigilante/urls.py
+++ vigilante/urls.py
@@ -19,5 +19,4 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('hood.urls')),
-    path('accounts/', include('registration.backends.simple.urls'))
 ]
