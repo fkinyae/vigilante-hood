afa6101635074fa87eddb5836bd9684bbf5f306d
Francis Kinyae <fmkinyae@gmail.com>
2021-09-24 10:55:52 +0300

    change settings for the login & logout views

--- vigilante/settings.py
+++ vigilante/settings.py
@@ -138,6 +138,5 @@ EMAIL_PORT = 587
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/' 
+LOGIN_REDIRECT_URL = 'welcome'
+LOGOUT_REDIRECT_URL = 'login'
