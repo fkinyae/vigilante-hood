2c9772ff47bdc42c5f52432b87fc3ad04006f5fb
Francis Kinyae <fmkinyae@gmail.com>
2021-09-24 19:22:26 +0300

    add form widgets for neighbourhood form

--- hood/forms.py
+++ hood/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth.models import User
-from .models import Profile
+from django.forms.widgets import NumberInput, TextInput, Textarea
+from .models import Profile, NeighbourHood
 
 
 #usersignup form
@@ -23,3 +24,24 @@ class ProfileUpdateForm(forms.ModelForm):
             class Meta:
                 model=Profile
                 fields = ['bio','location','hood_name','profile_pic']        
+                
+class NeighbourHoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighbourHood
+        exclude = ('admin',)   
+        fields = ['hood_pic', 'name', 'locality', 'occupants_count', 'police_call', 'hospital_call', 'Fire_call' ]   
+        
+        widgets = {
+            'name' : TextInput(attrs={'class' : 'form-control', 'placeholder' : 'name of neighbourhood'}),
+            'locality' : TextInput(attrs={'class' : 'form-control', 'placeholder' : 'Location'}),
+            'occupants_count' : NumberInput(attrs={'class' : 'form-control', 'placeholder' : 'Occupants Count'}),
+            'police_call' : NumberInput(attrs={'class' : 'form-control', 'placeholder' : 'Police Department'}),
+            'hospital_call' : NumberInput(attrs={'class' : 'form-control', 'placeholder' : 'Hospital Contact'}),
+            'Fire_call' : NumberInput(attrs={'class' : 'form-control', 'placeholder' : 'Fire Department Tel.'}),
+
+
+
+
+
+        }
+    
