0fcbfe7a0f37f3cc120159499906deb59b43c95a
Francis Kinyae <fmkinyae@gmail.com>
2021-09-24 10:54:40 +0300

    add password change url

--- hood/urls.py
+++ hood/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    #path('', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
     path('',views.welcome, name='welcome'),
     path('signup/', views.usersignup, name='signup'),
     path('activate/<uidb64>/<token>/',views.activate_account, name='activate'),
     path('registration/', include('django_registration.backends.activation.urls')),
     path('registration/', include('django.contrib.auth.urls')),
+    path('password_change/', views.PasswordsChangeView.as_view(), name='password_change'),
     
+
+ 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
