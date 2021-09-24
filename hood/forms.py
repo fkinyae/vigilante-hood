f0ba128f3c1bfd319ac84bb16f655ed5b7b9c581
Francis Kinyae <fmkinyae@gmail.com>
2021-09-24 13:50:57 +0300

    add forms for profile and changing profile

--- hood/forms.py
+++ hood/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 #usersignup form
@@ -10,3 +11,15 @@ class UserSignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        
+        
+class UserUpdateForm(forms.ModelForm):
+            class Meta:
+                model=User
+                fields = ['username','email','first_name','last_name']                
+       
+       
+class ProfileUpdateForm(forms.ModelForm):
+            class Meta:
+                model=Profile
+                fields = ['bio','location','hood_name','profile_pic']        
